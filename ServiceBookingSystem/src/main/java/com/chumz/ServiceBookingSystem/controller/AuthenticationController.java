a2c89a1706c631f6bb2d2fb344082a4aeb6aed12
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-13 14:43:38 +0200

    Update AuthenticationController.java

--- ServiceBookingSystem/src/main/java/com/chumz/ServiceBookingSystem/controller/AuthenticationController.java
+++ ServiceBookingSystem/src/main/java/com/chumz/ServiceBookingSystem/controller/AuthenticationController.java
@@ -3,20 +3,27 @@ package com.chumz.ServiceBookingSystem.controller;
 import com.chumz.ServiceBookingSystem.dto.AuthenticationRequest;
 import com.chumz.ServiceBookingSystem.dto.SignupRequestDTO;
 import com.chumz.ServiceBookingSystem.dto.UserDto;
+import com.chumz.ServiceBookingSystem.entity.User;
 import com.chumz.ServiceBookingSystem.repository.UserRepository;
 import com.chumz.ServiceBookingSystem.services.authentication.AuthService;
 import com.chumz.ServiceBookingSystem.services.jwt.UserDetailsServiceImpl;
 import com.chumz.ServiceBookingSystem.util.JwtUtil;
 import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class AuthenticationController {
@@ -57,12 +64,25 @@ public class AuthenticationController {
     }
 
     public void createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest,
-                                          HttpServletResponse response) {
+                                          HttpServletResponse response) throws IOException, JSONException {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                    authenticationRequest.getUsername()
-            ))
+                    authenticationRequest.getUsername(), authenticationRequest.getPassword()
+            ));
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Incorrect username or password", e);
         }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails.getPassword());
+        User user = userRepository.findFirstByEmail(authenticationRequest.getUsername());
+
+        response.getWriter().write(new JSONObject()
+                .put("userId", user.getId())
+                .put("role", user.getRole())
+                .toString()
+        );
 
     }
 }
